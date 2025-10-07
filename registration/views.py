296ecc8edaf054ce6f8a1e00159c6232d05881e5
Saiganity1 <sicatmichaeldave0411@gmail.com>
2025-10-07 12:15:44 +0800

    ADDED VIEW LIST

--- registration/views.py
+++ registration/views.py
@@ -14,3 +14,9 @@ def register_user(request):
         serializer.save()
         return Response(serializer.data, status=status.http_201_created)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def list_users(request):
+    users = UserRegistration.objects.all()
+    serializer = RegistrationSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
